04ac608b94137103a6bfa415e253f3d2dac539f1
Jedd de Gouveia <jedd@moogah.net>
2024-06-27 21:50:30 +1000

    Add setup file for unit tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
         include: ['**/*.{test,spec}.{ts,tsx}'],
         exclude: ['./node_modules', './dist'],
         css: false,
+        setupFiles: ['./src/setup.ts'],
         coverage: {
             exclude: configDefaults.coverage.exclude,
             all: true,
